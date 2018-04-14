adabe55b49df25043155f6c2fe12ab83c6424967
cowin <Cow_1997@163.com>
2018-04-14 13:07:01 +0800

    second

--- app/src/main/java/com/example/admin/testintentconfig/MainActivity.java
+++ app/src/main/java/com/example/admin/testintentconfig/MainActivity.java
@@ -1,13 +1,33 @@
 package com.example.admin.testintentconfig;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.GridLayout;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        Button button=new Button(this);
+        button.setText("Activity2");
+        LinearLayout linearLayout=new LinearLayout(this);
+        linearLayout.addView(button);
+        setContentView(linearLayout);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent();
+                intent.setAction("ActionTest");
+                intent.addCategory("Category");
+                intent.addCategory("Category1");
+                startActivity(intent);
+            }
+        });
     }
 }
